313ffa313089ea0d4ae7ae2f62239176169f83a2
Валерия <@DESKTOP-4NOAAIP>
2024-10-01 08:49:29 +0300

    lab 2

--- lab-2/lab-2.cpp
+++ lab-2/lab-2.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
 int main() {
+	setlocale(LC_ALL, "ru");
+	double a, b, S, P;
+
+	cout << "Введите сторону a: ";
+	cin >> a;
+	cout << "Введите сторону b: ";
+	cin >> b;
+
+	// Расчет площади
+	S = a * b;
+	// Расчет периметра
+	P = 2 * (a + b);
+
+
+	cout << "Площадь прямоугольника: " << S << endl;
+	cout << "Периметр прямоугольника: " << P << endl;
 
 }
